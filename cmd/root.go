76e3528164911f64e3cc2436c20ec44b29cde77a
Milana <milana.stojadinov@gmail.com>
2017-07-09 19:46:52 +0200

    Add description

--- cmd/root.go
+++ cmd/root.go
@@ -27,16 +27,9 @@ var cfgFile string
 // RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:   "uberdo",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	Short: "uberdo is a todo application",
+	Long: `uberdo will try to help you get more done in less time.
+It is as simple as possible and helps you accomplish your goals.`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -48,7 +41,7 @@ func Execute() {
 	}
 }
 
-func init() { 
+func init() {
 	cobra.OnInitialize(initConfig)
 
 	// Here you will define your flags and configuration settings.
